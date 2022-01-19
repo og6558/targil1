52b62c46ce8043a73dda26fd09cbe0657c41eed1
og6558 <og6558@bs.amalnet.k12.il>
2022-01-19 22:46:20 +0200

    commit 1

--- app/src/main/java/com/example/targil1/MainActivity.java
+++ app/src/main/java/com/example/targil1/MainActivity.java
@@ -3,14 +3,23 @@ package com.example.targil1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    Button btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        btn = findViewById(R.id.btn);
 
+
+    }
+
+    public void doron(View view) {
+        btn.setText("Oh, yea, I’ve been clicked!");
     }
 }
